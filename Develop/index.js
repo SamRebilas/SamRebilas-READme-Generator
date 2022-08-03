44ffa298582995ffcf7e96c60bfa6c6fb4347c2e
Samrebilas <sam.rebilas@gmail.com>
2022-08-03 10:38:00 -0400

    included required packages and installed npm

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs')
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -11,3 +12,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
