2de1b85a00dffd3c5a96354551dee7f5908bf5cc
Samrebilas <sam.rebilas@gmail.com>
2022-08-05 11:35:07 -0400

    included questions, init function now works, and index file now links to markdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,59 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs')
+const generateMarkdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [  
+    //title question
+    {
+    type: "input",
+    name: "projectTitle",
+    message: "What is the title of your project?"
+
+    },
+//project description
+{
+    type: "input",
+    name: "projectDescription",
+    message: "Enter a description of your project."
+}, 
+{
+    type: "input",
+    name: "projectInstall",
+    message: "Enter installation instruction for your project."
+},
+{
+    type: "input",
+    name: "usage",
+    message: "Enter usage instructions for your project."
+},
+{
+    type: "list",
+    name: "license",
+    message: "What license will you use?",
+    choices: ['yes', 'no']
+},
+{
+    type: "input",
+    name: "projectContribution",
+    message: "Enter a description for contributions."
+},
+{
+    type: "input",
+    name: "testing",
+    message: "Enter instructions for running a test."
+}
+]
+;
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+  
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
